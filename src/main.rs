45c5a6cf896ac054b3bbf2aab35517822e564301
lunispang <linus.wennborn@outlook.com>
2024-05-19 20:16:16 +0200

    fix silly little bug😊

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //width: 40, height: 20, size: 800;
 //positions as u16, 0x00ff as x, 0xff00 as y
 //pos 0 in bottom left
-//x-> y ^
+//x-> y V
 
 use inputbot::KeybdKey::{self, *};
 use rand::prelude::*;
@@ -37,23 +37,17 @@ fn print_state(apple : u16, snake : Vec<u16>) {
     }
 }
 
-fn move_apple(snake : &Vec<u16>, apple : &mut u16){
+fn move_apple(snake : Vec<u16>, apple : &mut u16){
     let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-    let mut n: usize = rng.gen_range(0..(800-snake.len()));
-    for x in 0..40 {
-        for y in 0..20 {
-            if n == 0 {
-                *apple = get_pos(x, y).clone();
-            }
-            if !snake.contains(&get_pos(x, y)) {
-                n-=1;
-            }
-        }
+    let mut pos: u16 = get_pos(rng.gen_range(0..40), rng.gen_range(0..20));
+    while snake.contains(&pos) {
+        pos = get_pos(rng.gen_range(0..40), rng.gen_range(0..20));
     }
+    *apple = pos;
 }
 
 fn add_dir(pos: u16, dir: u8) -> u16 {
-    match dir{
+    match dir {
         0 => pos-0x100,
         1 => pos+1,
         2 => pos+0x100,
@@ -72,17 +66,17 @@ fn move_snake(snake : &mut Vec<u16>, head_dir: u8, apple : &mut u16) -> bool {
     }
     snake.push(newheadpos);
     if newheadpos != *apple {
-        move_apple(snake, apple);
         snake.remove(0);
+        return true;
     }
+    move_apple(snake.to_vec(), apple);
     return true;
 }
 
 fn main() {
     let mut apple: u16 = get_pos(10, 10);
     let mut snake: Vec<u16> = Vec::new();
-    let mut running: bool = true;
-    let tick = 50;
+    let tick = 1000;
     // 0 -> up
     // 1 -> right
     // 2 -> down
@@ -93,18 +87,18 @@ fn main() {
     for x in 3..6 {
         snake.push(get_pos(x, 10));
     }
-    while running {
+    loop {
         if exit_key.is_pressed(){ 
-            running = false;
+            break;
         }
         let head_pos: u16 = *snake.last().unwrap();
         for key in 0..4 {
-            if movement_keys[key].is_pressed() &! snake.contains(&add_dir(head_pos, head_dir)){
+            if movement_keys[key].is_pressed() &! snake.contains(&add_dir(head_pos, key as u8)){
                 head_dir = key as u8;
             }
         }
         if !move_snake(&mut snake, head_dir, &mut apple) {
-            running = false;
+            break;
         }
         print_state(apple, snake.clone());
         std::thread::sleep(std::time::Duration::from_millis(tick));
