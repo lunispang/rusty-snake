7dee36117db71a2266cba7a8311c180057687bc6
lunispang <linus.wennborn@outlook.com>
2024-05-19 20:28:52 +0200

    small fixes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@
 use inputbot::KeybdKey::{self, *};
 use rand::prelude::*;
 
+const YSIZE: u8 = 10;
+const XSIZE: u8 = 20;
+
 fn get_pos(x: u8, y: u8) -> u16 {
     return (x as u16) + ((y as u16) << 8);
 }
@@ -19,15 +22,16 @@ fn get_y(pos: u16) -> u8 {
 }
 
 fn print_state(apple : u16, snake : Vec<u16>) {
+    print!("\x1B[2J");
     let mut current_line : [char; 40];
     let mut current_str : String;
     let apple_y: u8 = get_y(apple);
-    for y in 0..20 {
+    for y in 0..YSIZE {
         current_line = [' '; 40];
         if apple_y == y {
             current_line[get_x(apple) as usize] = 'O';
         }
-        for x in 0..40 {
+        for x in 0..XSIZE {
             if snake.contains(&get_pos(x, y)) {
                 current_line[x as usize] = '#';
             }
@@ -39,9 +43,9 @@ fn print_state(apple : u16, snake : Vec<u16>) {
 
 fn move_apple(snake : Vec<u16>, apple : &mut u16){
     let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
-    let mut pos: u16 = get_pos(rng.gen_range(0..40), rng.gen_range(0..20));
+    let mut pos: u16 = get_pos(rng.gen_range(0..XSIZE), rng.gen_range(0..YSIZE));
     while snake.contains(&pos) {
-        pos = get_pos(rng.gen_range(0..40), rng.gen_range(0..20));
+        pos = get_pos(rng.gen_range(0..XSIZE), rng.gen_range(0..YSIZE));
     }
     *apple = pos;
 }
@@ -61,7 +65,7 @@ fn add_dir(pos: u16, dir: u8) -> u16 {
 fn move_snake(snake : &mut Vec<u16>, head_dir: u8, apple : &mut u16) -> bool {
     let headpos: u16 = *snake.last().unwrap();
     let newheadpos: u16 = add_dir(headpos, head_dir);
-    if snake.contains(&newheadpos) || get_x(newheadpos) >= 40 || get_y(newheadpos) >= 20{
+    if snake.contains(&newheadpos) || get_x(newheadpos) >= XSIZE || get_y(newheadpos) >= YSIZE {
         return false;
     }
     snake.push(newheadpos);
@@ -74,9 +78,9 @@ fn move_snake(snake : &mut Vec<u16>, head_dir: u8, apple : &mut u16) -> bool {
 }
 
 fn main() {
-    let mut apple: u16 = get_pos(10, 10);
+    let mut apple: u16 = get_pos(XSIZE/2, YSIZE/2);
     let mut snake: Vec<u16> = Vec::new();
-    let tick = 1000;
+    let tick = 200;
     // 0 -> up
     // 1 -> right
     // 2 -> down
@@ -85,7 +89,7 @@ fn main() {
     let movement_keys : [KeybdKey; 4] = [WKey, DKey, SKey, AKey];
     let exit_key : KeybdKey = EscapeKey;
     for x in 3..6 {
-        snake.push(get_pos(x, 10));
+        snake.push(get_pos(x, YSIZE/2));
     }
     loop {
         if exit_key.is_pressed(){ 
